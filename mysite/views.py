420a2a072c621c5dd4a9075730393127cac01162
nitin rajesh <nitinrajesh094@gmail.com>
2020-10-28 22:31:16 +0530

    creating home page

--- mysite/views.py
+++ mysite/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request,'basic.html')
+    return render(request,'home.html')
 
 def about(request):
     return render(request,'tut17.html')
