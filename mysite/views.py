7e8844e1c6c5a0d55dcf836a43ae58fb4d246841
nitin rajesh <nitinrajesh094@gmail.com>
2020-10-28 12:49:05 +0530

    added static files and basic.html

--- mysite/views.py
+++ mysite/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request,'index.html')
+    return render(request,'basic.html')
 
 def about(request):
     return render(request,'tut17.html')
